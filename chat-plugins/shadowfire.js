eb57381d6a43db0b2308f6ac128fab2b02bd7873
Steelchar <steelchar90@gmail.com>
2015-10-29 17:43:55 -0700

    Add /restart

--- chat-plugins/shadowfire.js
+++ chat-plugins/shadowfire.js
@@ -15,7 +15,23 @@ var badges = fs.createWriteStream('badges.txt', {
 	'flags': 'a'
 });
 exports.commands = {
-	// Shingeki no Kyojin
+		restart: function(target, room, user) {
+		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever');
+		} catch (e) {
+			return this.sendReply("/restart requires the \"forever\" module.");
+		}
+		if (!Rooms.global.lockdown) {
+			return this.sendReply("For safety reasons, /restart can only be used during lockdown.");
+		}
+		if (CommandParser.updateServerLock) {
+			return this.sendReply("Wait for /updateserver to finish before using /restart.");
+		}
+		this.logModCommand(user.name + ' used /restart');
+		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
+},
 	arlert: 'alert',	
 	alert: function(target, room, user) {
 		if (!this.can('declare')) return false;
